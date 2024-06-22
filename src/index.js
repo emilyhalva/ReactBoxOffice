5702af42c4d24cf1edcbe5e347187e6c94631bf2
Emily Halva <ekhalva@gmail.com>
2024-06-22 14:34:49 -0700

    Implemented ability to add messages to rating scale

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size="24" color="pink" classname="test" />
   </React.StrictMode>
 );
